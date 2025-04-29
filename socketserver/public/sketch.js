efeb9f89c0b5f3df6b3e332a39ac76bb2d943d13
GustavP22R <gustavservilieri@gmail.com>
2025-04-29 14:58:06 +0200

    Frontend - GUI version 1
    Første version af GUI, med det minimum implementeret

--- socketserver/public/sketch.js
+++ socketserver/public/sketch.js
@@ -1,12 +1,25 @@
 var socket;
 var recievedMessage
 
+//Input and message positions
+xInput = 50;
+yInput = 110;
+
+xSend = 220;
+ySend = 50;
+
+//Titel positions
+xMessageBox = xInput - 5;
+
+XInsertMessage = xInput - 6;
+
+
 function preload(){
 
 }
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth - 30, windowHeight - 30);
   background(50);
 
   //connects to localhost
@@ -14,18 +27,71 @@ function setup() {
   socket.on('mouse', NewDrawing);
   
   //Input and message sending
-  messageInput = createInput("")
+  messageInput = createElement('textarea')
+  messageInput.attribute('placeholder','Message here')
+  messageInput.position(xInput , yInput)
+  messageInput.size(280,160);
+
   messageSend = createButton("Send message")
-  messageSend.position(10, 100)
+  messageSend.position(xSend, ySend)
   messageSend.mousePressed(sendData)
+  
+
+  NewMessage = createButton("New Message")
+  NewMessage.position(xSend, ySend + 30)
+  NewMessage.mousePressed(sendData)
+  NewMessage.size(105, 20);
 }
 
 function NewDrawing(data){
   recievedMessage = data.message
 }
 function draw() {  
-  background(100, 100, 100);
-  text(recievedMessage ,10, 10)
+  //Background design
+  background(3, 22, 52);
+  fill(3, 101, 100);
+  rect(20, 15, windowWidth - 70, windowHeight - 58, 30);
+  fill(3, 54, 73)
+  rect(20, windowHeight/2 - 15, windowWidth - 70, windowHeight/2 - 28, 0,0,30,30);
+
+  //Boxes
+  fill(131, 163, 163)
+  rect(xInput - 20, 25,310,260,30);
+  fill(255)
+  textSize(16);
+  textStyle(BOLD)
+  text("Send message\nbelow",120,60)
+
+  for(var i = 1; i < 5; i++)
+    {
+    fill(131, 163, 163)
+    rect((xInput + 75) + 220 * i, 25,215,260,30);
+    fill(0)
+    textSize(16);
+    fill(131, 163, 163)
+    rect((xInput + 75) + 220 * i, windowHeight/2,215,260,30);
+
+    fill(255);
+    rect(xInput + 87 + 220 * i, 104,190,160)
+    rect(xInput + 87 + 220 * i, windowHeight/2 + 80, 190, 160)
+    }
+
+  fill(131, 163, 163)
+  rect(xInput - 20, windowHeight/2,310,260,30);
+
+  fill(255);
+  textAlign(CENTER)
+  text("First message sent",230 + 220 * 1,70)
+  text("Error Control Code",230 + 220 * 2,70)
+  text("Encrypted",230 + 220 * 3,70)
+  text("Final message sent",230 + 220 * 4,70)
+
+  text("First message recieved",230 + 220 * 1,360)
+  text("Error Control Code",230 + 220 * 2,360)
+  text("Encrypted",230 + 220 * 3,360)
+  text("Final message recieved",230 + 220 * 4,360)
+
+  //text(recievedMessage ,10, 10)
 
 }
 
